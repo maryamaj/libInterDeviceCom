4390a33e8a1ccf1a588a2e9ba3a17800584729e9
Tommaso <tommaso.piazza@gmail.com>
2012-03-12 17:36:05 +0100

    Initial Commit

---
+++ InterDeviceCom/InterDeviceCom.h
@@ -0,0 +1,13 @@
+//
+//  InterDeviceCom.h
+//  InterDeviceCom
+//
+//  Created by Tommaso Piazza on 3/12/12.
+//  Copyright (c) 2012 ChalmersTH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InterDeviceCom : NSObject
+
+@end
